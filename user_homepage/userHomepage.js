d6830cbdb0278d68afc3a7c4cf018883b474ebb6
Justine Toong <programmerjustine@gmail.com>
2025-03-03 11:27:55 +0800

    Fixed container size

--- user_homepage/userHomepage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.add('active');
-});
-
-document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.remove('active');
-});
