ae0b49d0c90dff779d2f6770034663454170e87b
Justine Toong <programmerjustine@gmail.com>
2025-03-03 11:06:34 +0800

    User Homepage Feature

---
+++ userHomepage/userHomepage.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.add('active');
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.remove('active');
+});
